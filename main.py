9370f937935775ab343829db41c6e014dc820ecd
Andrey Konovalenko <andreinew@mail.ru>
2024-01-30 15:45:00 +0300

    chore: added miltiple file parsing logic

--- main.py
+++ main.py
@@ -3,23 +3,27 @@ import os
 
 directory = os.environ['directory']
 arr = os.listdir(rf'{directory}')
-file_name = arr[0]
-print(file_name)
-print(os.path.join(directory, arr[0]))
+
+def parser(file_name: str) -> None:
+    print(os.path.join(directory, file_name))
+    domtree = xml.dom.minidom.parse(os.path.join(directory, file_name))
+    group = domtree.documentElement
+
+    person = group.getElementsByTagName("NAME")
+    if (person.length > 0):
+        print(group.getElementsByTagName('LAST_NAME')[0].childNodes[0].nodeValue)
+    
+    business = group.getElementsByTagName('BUSINESS')
+    if (business.length > 0):
+        print(group.getElementsByTagName('FULL_NAME')[0].childNodes[0].nodeValue)
+
+    loans = group.getElementsByTagName('LOAN')
+    for loan in loans:
+        print(loan.getElementsByTagName('UUID').length)
+        # print(loan.getElementsByTagName('UUID')[0].childNodes[0].nodeValue)
+        # print(loan.getElementsByTagName('CREDIT_LIMIT')[0].childNodes[0].nodeValue)
 
 
-domtree = xml.dom.minidom.parse(os.path.join(directory, arr[0]))
 
-group = domtree.documentElement
-print(group)
-
-loans = group.getElementsByTagName('LOAN')
-for loan in loans:
-    print(loan.getElementsByTagName('UUID')[0].childNodes[0].nodeValue)
-    print(loan.getElementsByTagName('CREDIT_LIMIT')[0].childNodes[0].nodeValue)
-    # print(f'-- loan  {loan.getAttribute('UUID')}')
-    # print(f'-- loan  {loan.getAttribute('CREDIT_LIMIT')}')
-
-
-# for child in root: 
-#     print(child.tag, child)
+for file in arr:
+    parser(file)
