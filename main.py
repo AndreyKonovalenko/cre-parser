a89cc1f698b88046da7caa833a4fcad61396abf3
Andrey Konovalenko <andreinew@mail.ru>
2024-02-05 17:19:52 +0300

    chore: added new xml fialds info

--- main.py
+++ main.py
@@ -26,17 +26,82 @@ arr = os.listdir(rf'{directory}')
 # - Обнаружен факт возникновения просроченной задолженности по активным счетам длительностью 30 (тридцать) 
 # и более календарных дней в кредитных и иных организациях, включая Банк, 
 # максимальная сумму просрочки по которым превышала 10 000 руб.;
-# Обнаружен факт возникновения просроченной задолженности по закрытыми счетам 
+# Обнаружен факт возникновения просроченной задолженности по ызакрытыми счетам 
 # (дата закрытия счета не превышает 36 месяцев от даты подачи заявки)  длительностью 90 (девяносто) и более календарных дней 
 # на сумму более 10 000 руб. в кредитных и иных организациях включая Банк. 
 
 
+
+def uuidHandler(loan):
+    uuidTag = loan.getElementsByTagName('UUID')
+    if uuidTag.length > 0:
+        uuid = uuidTag[0].childNodes[0].nodeValue
+        print(f'uuid: {uuid}')
+    else:
+        print(f'uuid: не задан')
+            
+def statusHandler(loan):
+    statusTag = loan.getElementsByTagName('STATUS')
+    if statusTag.length > 0:
+        status = statusTag[0].childNodes[0].nodeValue
+        print(status) 
+        if status == '00':
+            print('cтатус: активный')
+        if status == '13':
+            print('статус: закрыт')
+    else:
+        print("cтатус: не задан ")
+
+def currentDelayHandler(loan):
+    currentDelayTag = loan.getElementsByTagName('CURRENT_DELQ')
+    currentDelayBalanceTag = loan.getElementsByTagName('DELQ_BALANCE')
+    if (currentDelayTag.length > 0):
+        currentDelay = currentDelayTag[0].childNodes[0].nodeValue
+        currentDelayBalance = "0.00"
+        if(currentDelayBalanceTag.length > 0):
+            currentDelayBalance = currentDelayBalanceTag[0].childNodes[0].nodeValue
+            print(f'текущая просрочка дней: {currentDelay} дней, на сумму: {currentDelayBalance} ')
+        else:
+            print('просроченная задолженность отсутствует')
+
+def maxDeleyBalanceHandler(loan):
+    maxDeleyBalanceTag = loan.getElementsByTagName('MAX_DELQ_BALANCE')
+    if (maxDeleyBalanceTag.length > 0):
+        maxDeleyBalance = maxDeleyBalanceTag[0].childNodes[0].nodeValue
+        print(f'максимальная историческая просроска: {maxDeleyBalance}')
+
+def termminationReasonHandler(loan):
+    termminationReasonTag = loan.getElementsByTagName('TERMINATION_REASON')
+    if (termminationReasonTag.length > 0):
+        termminationReason = termminationReasonTag[0].childNodes[0].nodeValue
+        print(f'причена закрытия код: {termminationReason}')
+        if termminationReason == '1':
+            print('причина закрытия: ненадлежащее исполнение обязательств')
+        if termminationReason == '99':
+            print('причина закрытия: иное основаение')
+
+def deleyInfoHandler(loan):
+    delayLess5 = loan.getElementsByTagName('TTL_DELQ_5')[0].childNodes[0].nodeValue
+    delay5to29 = loan.getElementsByTagName('TTL_DELQ_5_29')[0].childNodes[0].nodeValue
+    delay30to59 = loan.getElementsByTagName('TTL_DELQ_30_59')[0].childNodes[0].nodeValue
+    delay60to89 = loan.getElementsByTagName('TTL_DELQ_60_89')[0].childNodes[0].nodeValue
+    delay90Plus = loan.getElementsByTagName('TTL_DELQ_90_PLUS')[0].childNodes[0].nodeValue
+   
+    if delayLess5 != '0' or delay5to29 != '0' or delay30to59 != '0' or delay60to89 != '0' or delay90Plus != '0':
+        print('Данные о просрочке:')
+        if delayLess5 != '0': print(f'менее 5 дней: {delayLess5}')
+        if delay5to29 != '0': print(f'5-29 дней: {delay5to29}')
+        if delay30to59 != '0': print(f'30-59 дней: {delay30to59}')
+        if delay60to89 != '0': print(f'60-89: {delay60to89}')
+        if delay90Plus != '0': print(f'Более 90: {delay90Plus}')
+    else: 
+        print('Историческая просрочка не обнаружена')
+
+
 def parser(file_name: str) -> None:
-    print(os.path.join(directory, file_name))
     domtree = xml.dom.minidom.parse(os.path.join(directory, file_name))
     group = domtree.documentElement
     
-
     person = group.getElementsByTagName("NAME")
     if (person.length > 0):
         print(group.getElementsByTagName('LAST_NAME')[0].childNodes[0].nodeValue)
@@ -47,56 +112,24 @@ def parser(file_name: str) -> None:
 
     loans = group.getElementsByTagName('LOAN')
 
-    for loan in loans:
-        uuidTag = loan.getElementsByTagName('UUID')
-        statusTag = loan.getElementsByTagName('STATUS')
-        currentDelayTag = loan.getElementsByTagName('CURRENT_DELQ')
-        currentDelayBalanceTag = loan.getElementsByTagName('DELQ_BALANCE')
+    for index, loan in enumerate(loans):
+        
         delayLess5 = loan.getElementsByTagName('TTL_DELQ_5')[0].childNodes[0].nodeValue
         delay5to29 = loan.getElementsByTagName('TTL_DELQ_5_29')[0].childNodes[0].nodeValue
         delay30to59 = loan.getElementsByTagName('TTL_DELQ_30_59')[0].childNodes[0].nodeValue
         delay60to89 = loan.getElementsByTagName('TTL_DELQ_60_89')[0].childNodes[0].nodeValue
         delay90Plus = loan.getElementsByTagName('TTL_DELQ_90_PLUS')[0].childNodes[0].nodeValue
-
-        if uuidTag.length > 0:
-            uuid = uuidTag[0].childNodes[0].nodeValue
-            print(f'uuid: {uuid}')
-        else:
-            print(f'uuid: не задан')
-            
-        if statusTag.length > 0:
-            status = statusTag[0].childNodes[0].nodeValue
-            print(status) 
-            if status == '00':
-                print('активный')
-            if status == '13':
-                print('закрыт')
-        else:
-            print("cтатус: на задан ")
-
-        if (currentDelayTag.length > 0):
-            currentDelay = currentDelayTag[0].childNodes[0].nodeValue
-            currentDelayBalance = "0.00"
-            if(currentDelayBalanceTag.length > 0):
-                currentDelayBalance = currentDelayBalanceTag[0].childNodes[0].nodeValue
-            print(f'текущая просрочка дней: {currentDelay} дней, на сумму: {currentDelayBalance} ')
-        else:
-            print('просроченная задолженность отсутствует')
-
        
         if delayLess5 != '0' or delay5to29 != '0' or delay30to59 != '0' or delay60to89 != '0' or delay90Plus != '0':
-            print('Данные о просрочке:')
-            if delayLess5 != '0': print(f'менее 5 дней: {delayLess5}')
-            if delay5to29 != '0': print(f'5-29 дней: {delay5to29}')
-            if delay30to59 != '0': print(f'30-59 дней: {delay30to59}')
-            if delay60to89 != '0': print(f'60-89: {delay60to89}')
-            if delay90Plus != '0': print(f'Более 90: {delay90Plus}')
-        else: 
-            print('Историческая просрочка не обнаружена')
-
-        # print(loan.getElementsByTagName('UUID')[0].childNodes[0].nodeValue)
-        # print(loan.getElementsByTagName('CREDIT_LIMIT')[0].childNodes[0].nodeValue)
-
+            print(f'====================N:{index+1}====================')
+            uuidHandler(loan)
+            statusHandler(loan)
+            currentDelayHandler(loan)
+            maxDeleyBalanceHandler(loan)
+            termminationReasonHandler(loan)
+            deleyInfoHandler(loan)
+            print('====================###====================')
+            print('')
 
 for file in arr:
     parser(file)
